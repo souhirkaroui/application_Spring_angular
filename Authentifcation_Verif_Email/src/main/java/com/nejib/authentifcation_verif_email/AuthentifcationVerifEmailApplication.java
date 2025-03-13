6dadbbbf76df9b5685492a73f7b429ed25b724c0
souhirkaroui <souhir.karoui.ks@gmail.com>
2025-03-13 13:10:51 +0100

    Update AuthentifcationVerifEmailApplication.java

--- Authentifcation_Verif_Email/src/main/java/com/nejib/authentifcation_verif_email/AuthentifcationVerifEmailApplication.java
+++ Authentifcation_Verif_Email/src/main/java/com/nejib/authentifcation_verif_email/AuthentifcationVerifEmailApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
-
 @EnableAspectJAutoProxy
 @RequiredArgsConstructor
 @ComponentScan(basePackages={"com.nejib.authentifcation_verif_email" ,"com.nejib.authentifcation_verif_email.CorsCongiguration"})
+@EnableJpaRepositories(basePackages = "com.nejib.authentifcation_verif_email.Repository")  // Ajout de l'annotation ici
 public class AuthentifcationVerifEmailApplication {
 
     public static void main(String[] args) {
@@ -19,3 +19,4 @@ public class AuthentifcationVerifEmailApplication {
     }
 
 }
+
